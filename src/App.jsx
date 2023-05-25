6a1cea8bb2741a7d5da1a49729586093dd150f88
vishnuSanskrit <jaimestevens68272@gmail.com>
2023-05-25 00:56:47 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+// dynamically calculate unqiue values here
+// ! For dynamically pass categories
+const categories = ['all', ...new Set(items.map((item) => item['category']))];
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+
+  // const categories = items.map((item) => item.category);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+    } else {
+      setMenuItems(items.filter((item) => item.category == category));
+    }
+  };
+
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>our menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Categories.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Categories = (props) => {
+  const { filterItems, categories } = props;
+
+  return (
+    <div className='btn-container'>
+      {categories.map((category) => {
+        return (
+          <button className='filter-btn' onClick={() => filterItems(category)}>
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
